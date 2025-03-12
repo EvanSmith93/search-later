f815b8ba2223f59632a3839c1a520d0902f0b441
EvanSmith93 <evan.smith993@gmail.com>
2025-03-12 02:54:33 -0600

    Clean up code and proxy frontend requests to the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,17 @@
 import express, { Request, Response } from "express";
-import fs from "fs";
-import path from "path";
 
 const app = express();
 const port = 4000;
 
-const filePath = "src/searchLater.md";
-
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   const search = req.query.search as string;
-  console.log(search, req.query);
 
-  if (search) {
-    const url = `https://google.com/search?q=${encodeURIComponent(search)}`;
-    const markdownLine = `* [${search}](${url})\n`;
-
-    fs.appendFileSync(filePath, markdownLine);
-
-    res.send(
-      `<script src="http://localhost:${port}/onSearchLater.js" id="on-search-later" data-search="${search}"></script>`
-    );
-  } else {
-    res.sendFile(path.join(__dirname, "../../app/dist"));
-  }
+  res.send(
+    `<script src="/onSearchLater.js" id="on-search-later" data-search="${
+      search ?? ""
+    }"></script>`
+  );
 });
-
-app.use(express.static("../app/dist"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

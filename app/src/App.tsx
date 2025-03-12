905336e3b540b8b541993e46859e1e4623012db2
EvanSmith93 <evan.smith993@gmail.com>
2025-03-12 03:13:27 -0600

    More UI updates

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 function App() {
   const [searches, setSearches] = useState<string[]>([]);
@@ -8,38 +10,40 @@ function App() {
   const getSearches = () => {
     const searches = localStorage.getItem("searches");
     if (searches) {
-      setSearches(JSON.parse(searches));
+      setSearches(JSON.parse(searches).reverse());
     }
   };
 
   const removeSearch = (index: number) => {
-    console.log("removeSearch", index);
     const newSearches = searches.filter((_, i) => i !== index);
     localStorage.setItem("searches", JSON.stringify(newSearches));
-    setSearches(newSearches);
+    setSearches([...newSearches].reverse());
   };
 
   const onClickLink = (search: string, index: number) => {
-    removeSearch(index);
-    console.log("redirect", search);
     const link = `https://google.com/search?q=${encodeURIComponent(search)}`;
     window.open(link, "_blank");
+
+    removeSearch(index);
   };
 
   return (
     <div className="p-12">
-      <h1 className="text-4xl mb-2">Search Later</h1>
+      <h1 className="text-4xl">Search Later</h1>
 
       {searches.map((search, index) => (
-        <li className="mb-2" key={index}>
-          <a
-            className="text-2xl underline cursor-pointer"
+        <div key={index}>
+          <Button
+            variant="ghost"
+            className="text-xl mt-2 cursor-pointer"
             onClick={() => onClickLink(search, index)}
           >
             {search}
-          </a>
-        </li>
+            <SquareArrowOutUpRight strokeWidth={2.5} />
+          </Button>
+        </div>
       ))}
+
       {searches.length === 0 && (
         <p className="text-xl text-gray-400">No searches saved</p>
       )}
